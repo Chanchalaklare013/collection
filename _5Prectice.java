5676f419b2e3b8d87f3005be40281637b65e5621
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-09-05 10:48:27 +0530

    Add files via upload

---
+++ _5Prectice.java
@@ -0,0 +1,15 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class _5Prectice 
+{
+ public static void main(String []args)
+ {
+    ArrayList<String> al=new ArrayList<>(Arrays.asList("java","c","c++","R"));
+    System.out.println(al);
+    
+    
+ }   
+}
